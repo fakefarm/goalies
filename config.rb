a10fd44876f64827aab0fe9857ed42c67f61e02b
Dave Woodall <dave.woodall@livingsocial.com>
2014-11-01 21:05:44 -0600

    Update nav

--- config.rb
+++ config.rb
@@ -76,9 +76,9 @@ activate :directory_indexes
 activate :livereload
 
 set :markdown_engine, :kramdown
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir,         'stylesheets'
+set :js_dir,          'javascripts'
+set :images_dir,      'images'
 
 configure :build do
   activate :minify_css
